1fe0f61a9861d9638d43ff5b7e547a4c10ad667a
Oleksandr Dushnyi <sdusnij@gmail.com>
2024-11-18 17:26:12 +0200

    fix: deleted unnecessary endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,24 +19,4 @@ export class UsersController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.updateUser(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
 }
