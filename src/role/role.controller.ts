e56566b0370ecc4936984ae4c538f569ccc7aaf4
Oleksandr Dushnyi <sdusnij@gmail.com>
2024-11-27 17:39:48 +0200

    fix: update role module and new endpoint

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -6,37 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { PostOwnershipGuard } from 'src/post/guards/postOwnership.guard';
 
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
-  // @Post()
-  // create(@Body() createRoleDto: CreateRoleDto) {
-  //   return this.roleService.create(createRoleDto);
-  // }
+  @Post()
+  @UseGuards(PostOwnershipGuard)
+  create(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.create(createRoleDto);
+  }
 
-  // @Get()
-  // findAll() {
-  //   return this.roleService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.roleService.findAll();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.roleService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.roleService.findOne(+id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-  //   return this.roleService.update(+id, updateRoleDto);
-  // }
+  @Patch(':id')
+  @UseGuards(PostOwnershipGuard)
+  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.roleService.update(+id, updateRoleDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.roleService.remove(+id);
-  // }
+  @Delete(':id')
+  @UseGuards(PostOwnershipGuard)
+  remove(@Param('id') id: string) {
+    return this.roleService.remove(+id);
+  }
 }
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RoleService {
@@ -21,5 +22,42 @@ export class RoleService {
 
   async getAllRoles() {
     return this.prisma.role.findMany();
+  }
+
+  async create(createRoleDto: CreateRoleDto) {
+    return this.prisma.role.create({
+      data: {
+        name: createRoleDto.name,
+        description: createRoleDto.description,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.role.findMany();
+  }
+
+  async findOne(id: number) {
+    const role = await this.prisma.role.findUnique({ where: { id } });
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} not found`);
+    }
+    return role;
+  }
+
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    const existingRole = await this.findOne(id);
+    return this.prisma.role.update({
+      where: { id: existingRole.id },
+      data: {
+        name: updateRoleDto.name,
+        description: updateRoleDto.description,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    const existingRole = await this.findOne(id);
+    return this.prisma.role.delete({ where: { id: existingRole.id } });
   }
 }
